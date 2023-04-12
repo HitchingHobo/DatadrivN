fb308cfb8ea15bccb7a980b881bde88406a293c8
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-04-12 12:39:22 +0200

    Working with JSONL files - test

---
+++ jsontest.py
@@ -0,0 +1,43 @@
+
+import json
+
+def dump_jsonl(data, output_path, append=False):
+    """
+    Write list of objects to a JSON lines file.
+    """
+    mode = 'a+' if append else 'w'
+    with open(output_path, mode, encoding='utf-8') as f:
+        for line in data:
+            json_record = json.dumps(line, ensure_ascii=False)
+            f.write(json_record + '\n')
+    print('Wrote {} records to {}'.format(len(data), output_path))
+
+def load_jsonl(input_path) -> list:
+    """
+    Read list of objects from a JSON lines file.
+    """
+    data = []
+    with open(input_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            data.append(json.loads(line.rstrip('\n|\r')))
+    print('Loaded {} records from {}'.format(len(data), input_path))
+    return data
+
+
+data = load_jsonl('2022.sample.jsonl')
+
+dist_list = []
+x = 0
+
+
+
+for row in data:
+    print(data[row])
+    print(type(row))
+    if any("och") in row:
+        dist_list.append(row)
+        x += 1
+    
+print(dist_list)
+print(x)
+
