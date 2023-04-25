51a089b8f8ad0599e16adb62735e1e72cbcaad04
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-04-25 14:59:32 +0200

    Update YP_test.py

--- YP_test.py
+++ YP_test.py
@@ -5,7 +5,7 @@ data = pd.read_csv('YP_med_texter.csv')
   
 # drop function which is used in removing or deleting rows or columns from the CSV files
 data.drop('concept_id', inplace=True, axis=1)
-data.drop('ar', inplace=True, axis=1)
+#data.drop('ar', inplace=True, axis=1)
 data.drop('datatyp', inplace=True, axis=1)
 data.drop('ssyk', inplace=True, axis=1)
 data.drop('geografi', inplace=True, axis=1)
@@ -14,3 +14,7 @@ data.drop('stycke3', inplace=True, axis=1)
 data = data.rename({'ingress':'Procent', 'stycke1':'Jobbmöjlighet', 'stycke2':'5 års sikt'}, axis='columns')
 
 print(data.head(4))
+
+#########
+# Ta bort år 2026, varannan rad
+#########
