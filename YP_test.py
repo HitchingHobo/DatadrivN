91d8878e13b4214d57b8d0ca8f53eddcb5119e76
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-04-25 14:31:56 +0200

    Yrkesprognos rensning

---
+++ YP_test.py
@@ -0,0 +1,16 @@
+import pandas as pd  
+  
+# read_csv function which is used to read the required CSV file
+data = pd.read_csv('YP_med_texter.csv')
+  
+# drop function which is used in removing or deleting rows or columns from the CSV files
+data.drop('concept_id', inplace=True, axis=1)
+data.drop('ar', inplace=True, axis=1)
+data.drop('datatyp', inplace=True, axis=1)
+data.drop('ssyk', inplace=True, axis=1)
+data.drop('geografi', inplace=True, axis=1)
+data.drop('stycke3', inplace=True, axis=1)
+
+data = data.rename({'ingress':'Procent', 'stycke1':'Jobbmöjlighet', 'stycke2':'5 års sikt'}, axis='columns')
+
+print(data.head(4))
