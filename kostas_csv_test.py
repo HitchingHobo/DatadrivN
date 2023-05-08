2f44b3911b3e9ad32d73383f5ac1dbd76615c4d0
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-05-08 14:43:46 +0200

    test upload

--- kostas_csv_test.py
+++ kostas_csv_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # read_csv function which is used to read the required CSV file
-data = pd.read_csv('jobtech_dataset2022.csv')
-data = data[['id', 'description', 'headline']]
+data = pd.read_csv('jobtech_temp2022Rall_UPDATED.csv', encoding=('UTF8'))
+data = data['id']
 
 print(data.head(4))
