a1488b02909b7fbd968a4e8ee44ae265372c2110
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2023-05-15 12:14:19 +0200

    Fixad kod för eng och sve

--- regelbaseratd_test.py
+++ regelbaseratd_test.py
@@ -3,7 +3,8 @@ from nltk.corpus import stopwords
 
 #pd.set_option('display.max_colwidth', None)
 
-gen_data = pd.read_csv('Lista Mask. och Fem. ord.csv', encoding=('UTF8'))
+gen_data = pd.read_csv('Lista Mask. och Fem. ord.csv', 
+                       encoding=('UTF8'))
 gen_data.dropna(inplace=True)
 
 mask_list = []
@@ -12,13 +13,16 @@ fem_list = []
 ## Sätter maskulina & feminina ord i listor
 for row in gen_data['Maskulint kodade ord']:
     mask_list.append(row)
+for row in gen_data['Masculinecoded words']:
+    mask_list.append(row)    
 for row in gen_data['Feminint kodade ord']:
+    fem_list.append(row)
+for row in gen_data['Femininecoded words']:
     fem_list.append(row)
 
 ## Importerar datan och preppar listor
-data = pd.read_csv('utvecklare_lista_svenska.csv',
-                    encoding=('UTF8'),
-                    nrows=100,)
+data = pd.read_csv('utvecklare_lista.csv',
+                    encoding=('UTF8'))
 
 mask_word_list=[]
 fem_word_list=[]
@@ -26,35 +30,42 @@ data['Mask_score'] = ''
 data['Fem_score'] = ''
 data['Mask_ord'] = ''
 data['Fem_ord'] = ''
+data['Annons_length'] = ''
 
 ## Sätter stopwords
 stopwords_list = stopwords.words('swedish')
+stopwords_list += stopwords.words('english')
 stopwords_list.extend(['academic', 'work', 'the', 'tiqqe', 'även'])
 
-## Hubudloop
+## Huvudloop
 for index in data.index:
     row = data['description.text'][index]
     row = str(row).lower()   
     mask_word_list=[]
-    neu_word_list=[]
     fem_word_list=[]
-    
+    x = 0
     for word in row.split():
+        x += 1
         if word not in stopwords_list:
             for i in mask_list:
                 if word.startswith(i):
                     mask_word_list.append(i) 
-                    print(mask_word_list)
+                    ##print(mask_word_list)
             for i in fem_list:
                 if word.startswith(i):
                     fem_word_list.append(i)
-                    print(fem_word_list)
+                    ##print(fem_word_list)
         else:
             continue
     data['Mask_ord'][index] = mask_word_list
     data['Mask_score'][index] = len(mask_word_list)
     data['Fem_ord'][index] = fem_word_list
     data['Fem_score'][index] = len(fem_word_list)
+    data['Annons_length'][index] = x
 
 
-print(data.sort_values(by=['Mask_score']))
+
+print(data[['employer.name', 'Mask_score', 'Fem_score', 'Annons_length']].head(30).sort_values(by=['Mask_score']))
+#data.to_csv('Final_output.csv'),
+
+data.info()
--- rensad_lista.py
+++ rensad_lista.py
@@ -6,10 +6,10 @@ from langdetect import detect
 data = pd.read_csv('jobtech_temp2022Rall_UPDATED.csv', 
                    encoding=('UTF8'))
 # data = data[['id', 'description.text', 'employer.name']]
-data.info()
+
 # data = pd.read_csv('jobtech_dataset2022.csv', encoding=('UTF8'), nrows=100)
 # data = data[['id', 'description', 'employer']]
-pd.set_option('display.max_colwidth', None)
+##pd.set_option('display.max_colwidth', None)
 data = data[ (data['occupation.label'].str.contains('Systemutvecklare|Mjukvaruutvecklare|Backend-utvecklare|Frontend-utvecklare|Applikationsutvecklare|Databasutvecklare', na=False))]
 # print(data[['occupation.label', 'employer.name', 'id', 'headline', 'access', 'experience_required', 'description.text', 'description.text_formatted', 'description.company_information', 'description.needs', 'description.requirements', 'employment_type.concept_id', 'employment_type.label', 'scope_of_work.min', 'scope_of_work.max', 'employer.organization_number', 'employer.name', 'employer.workplace', 'occupation.label', 'occupation_group.label', ]])
 
@@ -33,6 +33,7 @@ def is_english(text):
         return False
 
 def filter_csv(input_file, output_file):
+    x = 0
     with open(input_file, 'r', encoding=('UTF8')) as f_in, open(output_file, 'w', newline='', encoding=('UTF8')) as f_out:
         reader = csv.reader(f_in)
         writer = csv.writer(f_out)
@@ -40,18 +41,26 @@ def filter_csv(input_file, output_file):
             if len(row) == 0:
                 continue
             text = ' '.join(row).strip()
+            # print(text)
+            # print('##################################################################')
             if not is_english(text):
+                x += 1
                 writer.writerow(row)
-
+    #             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    #             print('Printad')
+    #             print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+    # print (x)
 # Example usage:
 input_file = 'Utvecklare_lista.csv'
 output_file = 'Utvecklare_lista_svenska.csv'
 filter_csv(input_file, output_file)
 
-
+data.info()
 # with open('Utvecklare_lista.csv', 'a', encoding="UTF8") as f:
 #     writer = csv.writer(f) #this is the writer object
 #     writer.writerow(column_name) # this will list out the names of the columns which are always the first entrries
 #     writer.writerow(data ['occupation.label' ]) #this is the data
 #     writer.writerow(data ['employer.name' ]) #this is the data
 #     writer.writerow(data ['description.text' ]) #this is the data
+
+
