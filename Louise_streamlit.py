744d63b5125e21e2bdfbb1dc20a83de3172b17f9
louisethorner <105272751+louisethorner@users.noreply.github.com>
2023-05-16 18:09:48 +0200

    Update Louise_streamlit.py

--- Louise_streamlit.py
+++ Louise_streamlit.py
@@ -8,26 +8,19 @@ st.header("MindMakerz")
 st.subheader("Lär dig att rekrytera kvinnliga utvecklare med vår tjänst!")
 
 # Create a text input box
-st.markdown(
-    """
-    <style>
-    .expanded-textarea .stTextInput textarea {
-        min-height: 200px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 col1, col2 = st.columns(2)
 with col1:
     st.subheader('Se hur könsneutral din annons är!')
     
     annons_input = st.text_input("Testa din annons här: ", "Klistra in här...")
+    st.write('Din annons har')
 with col2:
     st.subheader("Do's and dont's")
-    st.text("***Do's:***")
+    st.text("Do's:")
     manligt = st.checkbox('Kolla igenom de manligt betingade orden')
     check_lista = st.checkbox('Utseslut kravspecifikationer')
+    #fylla på här 
+    st.text("Dont's")
 
 
 
