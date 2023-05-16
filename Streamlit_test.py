6b88af8c72f0c8d5ccce81ca1e40a89aed46c8d8
louisethorner <105272751+louisethorner@users.noreply.github.com>
2023-05-16 14:58:14 +0200

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -10,7 +10,7 @@ def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
-    return r.jason()
+    return r.json()
 
 lottie_job_ad = load_lottieurl('https://assets3.lottiefiles.com/packages/lf20_g7ayoegc.json')
 
