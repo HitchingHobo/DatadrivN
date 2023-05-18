b54ec60d06195ad595c87c7da108bd14dc4c96fb
IamNorthman <norrman20@gmail.com>
2023-05-18 23:30:11 +0200

    Kolla annons

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -44,12 +44,46 @@ with st.container():
     # Read the CSV file
 df = pd.read_csv('Final_output.csv')
 
-# Get a random row index
-random_index = random.randint(0, len(df) - 1)
 
-# Get the random value
-random_value = df.iloc[random_index][0]  # Assuming the value is in the first column (index 0)
 
-# Display the random value using Streamlit
-with st.container(): 
-    st.write("Random Value:", random_value)
+def check_text(input_text, word_list):
+    matches = []
+    for col, word_column in enumerate(word_list):
+        for word in word_column:
+            if word.lower() in input_text.lower():
+                matches.append((word, col+1))
+    return matches
+
+# Load the CSV file
+word_data = pd.read_csv('Lista Mask. och Fem. ord.csv')
+
+# Convert each column to lowercase and store as a list of lists
+word_list = []
+for col in range(4):
+    words = word_data.iloc[:, col].astype(str).str.lower().tolist()
+    word_list.append(words)
+
+
+# Remove missing values ("nan") from the word list
+word_list = [[word for word in words if word != 'nan'] for words in word_list]
+
+
+# Streamlit app layout
+st.title("Kolla din annons om den innehåller något kodat ord")
+
+with st.container():
+    st.subheader("Skriv in din text:")
+    input_text = st.text_input("Text")
+
+    if st.button("Kolla!"):
+        matching_words = check_text(input_text, word_list)
+        if matching_words:
+            st.subheader("Matching keyword(s):")
+            for word, col in matching_words:
+                if col in [1, 3]:
+                    column_label = "Maskulint"
+                else:
+                    column_label = "Feminint"
+                st.write(f"Könskodat ord: {word}, {column_label}")
+        else:
+            st.write("Inga maskulint eller feminint kodade ord hittades.")
