a02ecbe4c04a53c346e3713a460f4ec95de39b2d
louisethorner <105272751+louisethorner@users.noreply.github.com>
2023-05-20 19:55:42 +0200

    vet inte vad detta den första är

--- Louise_streamlit.py
+++ Louise_streamlit.py
@@ -5,29 +5,30 @@ import altair as alt
 ## return [mask_word_list, fem_word_list, antal_ord]
 
 data = pd.read_csv('Final_output.csv')
-st.set_page_config(page_title='Mindmakerzzz', page_icon='', layout='wide')
+st.set_page_config(page_title='Annonskollen', page_icon='', layout='wide')
 
 st.header("MindMakerz")
 st.subheader("Lär dig att rekrytera kvinnliga utvecklare med vår tjänst!")
-
-# Create a text input box
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader('Se hur könsneutral din annons är!')
-    st.text("")
-    st.text("")
-    st.text("")
-    st.text("")
-    annons_input = st.text_input("Testa din annons här: ", "Klistra in här...")
+    annons_input = st.text_area('Testa hur könsneutral din annons är: ',
+                                height=250,
+                                placeholder='Klistra in här...')
     annons_results = testa_annons(annons_input)
-    st.write('Din annons har ', len(annons_results[0]), 'manliga')
+    
+# Create a text input box
 with col2:
-    st.subheader("Do's and dont's")
-    st.text("Do's:")
-    manligt = st.checkbox('Kolla igenom de manligt betingade orden')
-    check_lista = st.checkbox('Utseslut kravspecifikationer')
-    #fylla på här 
-    st.text("Dont's")
+    st.text("")
+    st.text("")
+    st.text("")
+    st.write('Din annons har ', len(annons_results[0]), 'manliga ord i sig')
+    st.write('De maskulint vinklade orden är: ')
+
+    for i in range(len(annons_results[0])):
+         st.write(annons_results[0][i])
+
+if annons_input:
+    st.subheader('Testa')      
 
 
 # col3, col4 = st.columns(2)
@@ -40,16 +41,15 @@ st.text("")
 st.text("")
 
 st.subheader('Vanligaste manliga orden i jobbannonser')
+barchart_data = pd.DataFrame(top_20_ord(), columns=['Ord', 'Antal'])
+#st.bar_chart(barchart_data, x='Antal', y='Ord')
 
 source = barchart_data
 
 base = alt.Chart(source).mark_bar().encode(
     x=alt.X('Antal'),
-    y=alt.Y('Ord', sort='-x'),
+    y=alt.Y('Ord', sort='-x' ),
     )
 
 
-st.altair_chart(base.mark_bar() + base.mark_text(align='left', dx=2), use_container_width=True)
-
-# Use the user input
-#st.write("You entered:", annons_input)
+st.altair_chart(base.mark_bar(), use_container_width=True)
