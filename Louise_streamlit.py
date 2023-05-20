5b1e772b99d7d8778068530fec11111ee9fb3fc0
louisethorner <105272751+louisethorner@users.noreply.github.com>
2023-05-20 21:40:28 +0200

    tillägg på streamlit-filen
    - testannons-grejen reagerar på om annonsen inte har manliga ord med ballonger och en kommentar
    - annars: visar manliga ord och antal
    - kort beskrivning av tjänsten

--- Louise_streamlit.py
+++ Louise_streamlit.py
@@ -2,13 +2,41 @@ import streamlit as st
 import pandas as pd
 from funktioner import *
 import altair as alt
+from PIL import Image 
 ## return [mask_word_list, fem_word_list, antal_ord]
 
 data = pd.read_csv('Final_output_sve.csv')
 st.set_page_config(page_title='Annonskollen', page_icon='', layout='wide')
 
-st.header("MindMakerz")
-st.subheader("Lär dig att rekrytera kvinnliga utvecklare med vår tjänst!")
+st.header("Annonskollen") 
+st.subheader("Lär dig att rekrytera kvinnliga utvecklare med vår tjänst! :computer:")
+# lägga in en liten bild här? 
+st.text("")
+st.text("")
+st.markdown("***Hur funkar det?***") #ändra färg på texten?
+st.markdown(
+    '''
+    Idag består techbranschen av **70% män**. Enligt kvalitativ data finns det en stor 
+    problematik kring att rekrytera kvinnliga utvecklare. 
+    Genom en databas med xxx jobbannonser har vi skapat en tjänst
+    för rekryterare som hjälper rekryterare att undvika manligt betingade ord i syfte
+    att attrahera kvinnliga kandidater. 
+
+    **Annonshjälpen erbjuder vägledning i form av att:**
+    - Få din annons analyserad
+    - Se de vanligaste manliga orden i jobbannonser
+    - Do's and dont's
+    
+
+    ''')
+
+
+
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+
 col1, col2 = st.columns(2)
 with col1:
     annons_input = st.text_area('Testa hur könsneutral din annons är: ',
@@ -21,14 +49,18 @@ with col2:
     st.text("")
     st.text("")
     st.text("")
-    st.write('Din annons har ', len(annons_results[0]), 'manliga ord i sig')
-    st.write('De maskulint vinklade orden är: ')
+    if len(annons_results[0]) < 1:
+        st.balloons() 
+        st.success('Bra jobbat! Din annons innehåller inga manliga ord.')
+        
+    else:
+       st.write('Din annons har ', len(annons_results[0]), 'manliga ord i sig')
+       st.write('De maskulint vinklade orden är: ')
+       for i in range(len(annons_results[0])):
+           st.write('-', annons_results[0][i])
 
-    for i in range(len(annons_results[0])):
-         st.write(annons_results[0][i])
-
-if annons_input:
-    st.subheader('Testa')      
+#if annons_input:
+ #   st.subheader('Testa')      
 
 
 # col3, col4 = st.columns(2)
